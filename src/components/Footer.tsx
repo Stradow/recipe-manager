774e2b99cb34723ae4bd053bc6a4574dcb89c2e1
Alexandra Domareski <aleexandra.junges@gmail.com>
2026-02-09 12:48:33 +0100

    recipe edit created

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="bg-[#F7F2F1] text-[#3A2A1A] text-center py-4">
-      <div className="border-t border-[#D28625] pb-4">
-        <p className="mt-8">&copy; 2026 Recipe Manager. All rights reserved.</p>
+    <footer className="fixed bottom-0 left-0 right-0 bg-[#F7F2F1] text-[#3A2A1A] text-center py-4 shadow-[0_-2px_8px_rgba(0,0,0,0.1)]">
+      <div className="border-t border-[#D28625] pt-4">
+        <p>&copy; 2026 Recipe Manager. All rights reserved.</p>
       </div>
     </footer>
   );
